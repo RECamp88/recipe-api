307b01406efdaaa4f43b1cbfc82f7dd43e4226ca
tetap semangat <trimcnichols@outlook.com>
2023-03-08 10:07:16 -0500

    account

--- src/main/java/Controller/RecipeController.java
+++ src/main/java/Controller/RecipeController.java
@@ -1,16 +1,20 @@
 package Controller;
 
+import Model.Account;
 import Model.Recipe;
+import Service.AccountService;
 import Service.RecipeService;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.javalin.Javalin; // update dependencies
 import io.javalin.http.Context;
 
 public class RecipeController {
     RecipeService recipeService;
-
+    AccountService accountService;
     public RecipeController(){
-
+        this.accountService = new AccountService();
         this.recipeService = new RecipeService();
     }
 
@@ -18,7 +22,36 @@ public class RecipeController {
     public Javalin startAPI(){
         Javalin app = Javalin.create();
         // create the endpoints here
+        app.post("/register", this::postAccountHandler);
+        app.post("/login", this::postLoginHandler);
+
+
         return app;
+    }
+    private void postAccountHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(ctx.body(), Account.class);
+        Account addedAccount = accountService.createAccount(account);
+        if(addedAccount!=null){
+            ctx.json(mapper.writeValueAsString(addedAccount));
+
+        }else{
+            ctx.status(400);
+        }
+
+    }
+
+
+    private void postLoginHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(ctx.body(), Account.class);
+        Account loginedAccount = accountService.loginAccount(account);
+        if(loginedAccount!=null){
+            ctx.json(mapper.writeValueAsString(loginedAccount));
+
+        }else{
+            ctx.status(401);
+        }
     }
 
 }
--- target/classes/Tables.sql
+++ target/classes/Tables.sql
@@ -1,4 +1,9 @@
 --h2 is typically used to setup a test database, not a prod database.
 --first, drop your tables (to reset your database for testing)
 --then create your tables
-afdbfdb
+drop table if exists account;
+create table account (
+    account_id int primary key auto_increment,
+    username varchar(255) unique,
+    password varchar(255)
+);
