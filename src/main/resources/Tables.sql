71cb7b40a590b52fb6f14b3d3ed6137a58043197
RECamp88 <recamp88@gmail.com>
2023-03-09 14:19:08 -0600

    Added create table account

--- src/main/resources/Tables.sql
+++ src/main/resources/Tables.sql
@@ -1,9 +1,15 @@
 drop table if exists recipe;
+drop table if exists account;
 
+create table account (
+    account_id int primary key auto_increment,
+    username varchar(255) unique,
+    password varchar(255)
+);
 create table recipe (
     recipe_id int primary key auto_increment,
     posted_by int,
     recipe_name varchar(255),
     main_ingredient varchar(25),
     foreign key (posted_by) references account(account_id)
-    );
+);
